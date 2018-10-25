891518b6fcde7b30878344b277c02c87f8129f1f
leoTlr <leo.teuchtler@hotmail.de>
2018-10-25 14:41:02 +0200

    move argparse and logging setup in seperate modules

---
+++ ressources/argparse_setup.py
@@ -0,0 +1,34 @@
+import argparse
+
+desc = """
+Host discovery tool.
+Will list and count all responding devices in given network by sending an
+UDP-packet to a closed port at each address in network and waiting for
+ICMP: Port unreachable responses.
+"""
+
+def setup_argparse():
+
+    parser = argparse.ArgumentParser(description=desc)
+    save_compare_group = parser.add_mutually_exclusive_group()
+    save_save_file_group = save_compare_group.add_mutually_exclusive_group()
+
+    parser.add_argument('ip_address', metavar='IPv4-address', help='IPv4 Address in CIDR-notation')
+    parser.add_argument('-nm', '--netmask', help='netmask in dotted decimal notation \
+                        (only needed if ip_address not in CIDR-notation)', metavar='NM')
+    parser.add_argument('-w', '--wait', help='define time (seconds) to wait for responses \
+                        after all packets are sent. Default 2', type=int, metavar='SEC')
+    parser.add_argument('-p', '--port', help='define a closed UDP port to use. Default 65333',
+                        type=int)
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help='only output number of discovered hosts (or number of changes since\
+                        save if -c is selected)')
+    save_save_file_group.add_argument('-s', '--save', action='store_true', help='save discovered hosts \
+                            as .xml file in ./saved_scans')
+    save_save_file_group.add_argument('-sf', '--save-file', metavar='PATH', help='like -s but save to provided \
+                            directory or file')
+    save_compare_group.add_argument('-c', '--compare', metavar='PATH', help='scan and compare with \
+                        saved hosts in file. If this is selected with -q, the number of\
+                        changes is returned')
+                        
+    return parser.parse_args()
---
+++ ressources/logging_setup.py
@@ -0,0 +1,23 @@
+import logging 
+
+# logging is used everywhere instead of print-statements
+# normal output is on loglevel INFO
+# normal output shall not have [INFO] prefix, other loglevels shall have prefix
+#  -> custom formatter
+class infoWithoutLevelPrefixFormatter(logging.Formatter):
+    """ different log formatting for loglevel INFO """
+    def format(self, record):
+        if record.levelno != logging.INFO:
+            record.msg = '[{}] {}'.format(record.levelname, record.msg)
+        return super().format(record)
+
+# needs to be run before main
+# can run after arguent parsing setup
+def setup_logging(log_level):
+
+    # create a custom handler, set its formatter to the custom formatter
+    handler = logging.StreamHandler()
+    handler.setFormatter(infoWithoutLevelPrefixFormatter())
+
+    # configure the logger to use the custom handler (arg as list because it needs to be an iterable)
+    logging.basicConfig(level=log_level, handlers=[handler])
